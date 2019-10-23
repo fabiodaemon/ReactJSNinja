4f083c04c8c1dea7084900acd67ec40853b1e0ed
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-10-22 22:23:29 -0300

    class mount/unmount incompleted

---
+++ src/timer.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class Timer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            time: 0
+        }
+    }
+
+componentDidMount () {
+    setInterval(() => {
+        this.setState({
+            time: this.state.time + 1
+        })
+    }, 1000)
+}
+
+render (){
+    return(
+        <div>Timer: {this.state.time}</div>
+    )
+  }
+}
+
+export default Timer;
